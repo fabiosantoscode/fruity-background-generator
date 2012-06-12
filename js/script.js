23363b217d8537f5dc947bd0449e8adb77a49ad7
Fábio Santos <fabiosantosart@gmail.com>
2012-06-12 23:56:43 +0100

    Doesn't crash on horizontal lines anymore. Used HTML5 validation to check the inputs before attempting to update the image; Stripes now update automatically when the parameter fields are written onto.

--- js/script.js
+++ js/script.js
@@ -16,30 +16,42 @@ jQuery(function($){
         }
         
         var self = {
-            preview_targets: $('#fsg-preview-img'), 
+            preview_targets: $('#fsg-preview-img'),
             inputs:{
-                angle:$(), // Empty selector
-                fg:$(),
-                bg:$(),
-                thickness:$(),
-                spacing:$(),
+                angle:$('#fsg-angle'), // These are default selectors. They can be overridden.
+                fg:$('#fsg-fg-color'),
+                bg:$('#fsg-bg-color'),
+                thickness:$('#fsg-thickness'),
+                spacing:$('#fsg-spacing'),
             },
+            event_namespace:'.fsg_event', //dot is there so we can remove namespacing
             update_cb: $.noop,
             update: function(){
+                if (!self.validate_parameters()){
+                    //return; // TODO: Add error message somewhere, user needs to know
+                }
+                var angle_deg = +self.inputs.angle.val();
+                
+                // Make the angle be between 0 and 179 where I can better work on it.
+                while (angle_deg < 0) angle_deg += 180;
+                while (angle_deg >= 180) angle_deg -=180;
                 
                 var spacing = +self.inputs.spacing.val(),
                     thickness = +self.inputs.thickness.val(),
                     required_thickness = spacing + thickness,
-                    angle = Math.abs(+self.inputs.angle.val() * Math.PI) / 180;
+                    angle = (angle_deg * Math.PI) / 180;
                 
-                
-                // Calculate required height and width for given angle:
-                var png_width = required_thickness / Math.cos(angle),
-                    png_height = png_width / Math.tan(angle);
-                
-                png_width = Math.round(png_width);
-                png_height = Math.round(png_height);
-                
+                if (angle_deg == 0){
+                    var png_width=required_thickness,
+                        png_height=1;
+                } else if (angle_deg == 90){
+                    var png_width=1,
+                        png_height=required_thickness;
+                } else {
+                    // Calculate required height and width for given angle:
+                    var png_width = Math.abs(Math.round(required_thickness / Math.cos(angle))),
+                        png_height = Math.abs(Math.round(png_width / Math.tan(angle)));
+                }
                 var png = new PNGlib(png_width,png_height,256);
                 
                 //alert([png_width,png_height,angle]);
@@ -64,31 +76,53 @@ jQuery(function($){
             set_update_callback: function(cb){
                 self.update_cb = cb;
             },
+            bind_input_events: function(){
+                $.each(self.inputs,function(){
+                    update_self=function(){self.update();};
+                    $(this).bind('input' + self.event_namespace, update_self);
+                    $(this).bind('change' + self.event_namespace, update_self);
+                });
+            },
+            unbind_input_events: function(){
+                $.each(self.inputs,function(){
+                    $(this).unbind('input' + self.event_namespace);
+                    $(this).unbind('change' + self.event_namespace);
+                });
+            },
             set_inputs: function(inputs){
+                unbind_input_events();
+                
                 self.inputs.angle=$(inputs.angle);
                 self.inputs.thickness=$(inputs.thickness);
                 self.inputs.spacing=$(inputs.spacing);
                 self.inputs.fg=$(inputs.fg);
                 self.inputs.bg=$(inputs.bg);
+                
+                bind_input_events();
             },
             set_preview_targets: function(target){
                 self.preview_targets = $(target);
             },
+            validate_parameters: function(){ // Return true if everything is valid
+                ret = true;
+                $('.fsg input')
+                    .each(function(){
+                        if (!this.checkValidity()){
+                            ret = false;
+                            return false;
+                        }
+                    });
+                return ret;
+            },
+            init: function(){
+                self.bind_input_events();
+                self.update();
+            }
         };return self;
     })();
     
     
-    stripe_creator.set_update_callback(function(){
-        //$('body').css('background','yellow');
-    });
-    
-    stripe_creator.set_inputs({
-        angle:'#fsg-angle',
-        fg:'#fsg-fg-color',
-        bg:'#fsg-bg-color',
-        thickness:'#fsg-thickness',
-        spacing:'#fsg-spacing'
-    });
+    stripe_creator.init();
     
     $('.apply')
         .click(stripe_creator.update);
@@ -107,9 +141,9 @@ jQuery(function($){
             }
             
             stripe_creator.update();
-        }).change();
+        });
     
-    $('.fsg .colors input[type="color"]')
+    $('.fsg .colors input.color')
         .change(function(){
             var color_hex = $(this).val();
             
